7fc2458b0b80eea57cf2ec91a869d41efb4cbc11
Luis Guzmán <luisguzman@MacBook-Pro-de-Luis.local>
2024-04-13 11:10:59 -0400

    chg: cambios en calculos para fuerza laboral total personal indirecto

--- src/components/daily-report/total-indirect-work-force.jsx
+++ src/components/daily-report/total-indirect-work-force.jsx
@@ -27,26 +27,60 @@ const TotalIndirectWorkForce = () => {
 
   const [totalIndirectWorkForce, setTotalIndirectWorkForce] = useState(initialState)
   const [indirectAccumulatedHours, setIndirectAccumulatedHours] = useState(0)
+  const [indirectAccumulatedOffered, setIndirectAccumulatedOffered] = useState(0)
+  const [indirectAccumulatedContracted, setIndirectAccumulatedContracted] = useState(0)
+  const [indirectAccumulatedCertified, setIndirectAccumulatedCertified] = useState(0)
+  const [indirectAccumulatedBreaked, setIndirectAccumulatedBreaked] = useState(0)
+  const [indirectAccumulatedWorked, setIndirectAccumulatedWorked] = useState(0)
+  const [indirectAccumulatedPrevious, setIndirectAccumulatedPrevious] = useState(0)
+  const [indirectAccumulatedActual, setIndirectAccumulatedActual] = useState(0)
 
   const onChangeData = (e) => {
     if (validate(e.target.value)) {
-      setTotalIndirectWorkForce({ ...totalIndirectWorkForce, [e.target.id]: e.target.value })
+      setTotalIndirectWorkForce({
+        // Después debe cambiar por el state unico
+        ...totalIndirectWorkForce,
+        indirectPreviusAccumulated: e.target.value,
+      })
+      setIndirectAccumulatedActual(Number(e.target.value) + Number(indirectAccumulatedHours))
     }
   }
 
   useEffect(() => {
     let hours = 0
+    let offered = 0
+    let contracted = 0
+    let certified = 0
+    let breaked = 0
+    let workekd = 0
     for (let indirectData of indirectWorkForceListContext) {
       hours = hours + Number(indirectData.actions.hh)
+      offered = offered + Number(indirectData.actions.offeredNumber)
+      contracted = contracted + Number(indirectData.actions.contractedNumber)
+      certified = certified + Number(indirectData.actions.certified)
+      breaked = breaked + Number(indirectData.actions.breakNumber)
+      workekd = workekd + Number(indirectData.actions.workNumber)
     }
     setIndirectAccumulatedHours(hours)
-    setTotalIndirectWorkForce({
-      ...totalIndirectWorkForce,
-      indirectCurrentAccumulated: hours,
-    })
+    setIndirectAccumulatedOffered(offered)
+    setIndirectAccumulatedContracted(contracted)
+    setIndirectAccumulatedCertified(certified)
+    setIndirectAccumulatedBreaked(breaked)
+    setIndirectAccumulatedWorked(workekd)
+    const data = {
+      indirectSubtotalOfferedNumber: offered,
+      indirectSubtotalContractedNumber: contracted,
+      indirectSubtotalCertifiedNumber: certified,
+      indirectSubtotalBreakNumber: breaked,
+      indirectSubtotalWorkNumber: workekd,
+      indirectSubstotalHHNumber: hours,
+    }
+
+    setTotalIndirectWorkForce(data)
   }, [indirectWorkForceListContext])
 
   useEffect(() => {
+    console.log('registro', totalIndirectWorkForce)
     storeTotalIndirectWorkForce(totalIndirectWorkForce)
   }, [totalIndirectWorkForce])
 
@@ -71,72 +105,60 @@ const TotalIndirectWorkForce = () => {
               <CFormInput
                 type="text"
                 id="indirectSubtotalOfferedNumber"
-                value={totalIndirectWorkForce.indirectSubtotalOfferedNumber || ''}
+                value={indirectAccumulatedOffered || '0'}
+                disabled
                 placeholder="N° Ofertado"
                 text=""
-                onChange={(e) => {
-                  onChangeData(e)
-                }}
               />
             </CTableDataCell>
             <CTableDataCell>
               <CFormInput
                 type="text"
                 id="indirectSubtotalContractedNumber"
-                value={totalIndirectWorkForce.indirectSubtotalContractedNumber || ''}
+                value={indirectAccumulatedContracted || '0'}
+                disabled
                 placeholder="N° Contratados"
                 text=""
-                onChange={(e) => {
-                  onChangeData(e)
-                }}
               />
             </CTableDataCell>
             <CTableDataCell>
               <CFormInput
                 type="text"
                 id="indirectSubtotalCertifiedNumber"
-                value={totalIndirectWorkForce.indirectSubtotalCertifiedNumber || ''}
+                value={indirectAccumulatedCertified || '0'}
                 placeholder="Acreditados"
+                disabled
                 text=""
-                onChange={(e) => {
-                  onChangeData(e)
-                }}
               />
             </CTableDataCell>
             <CTableDataCell>
               <CFormInput
                 type="text"
                 id="indirectSubtotalBreakNumber"
-                value={totalIndirectWorkForce.indirectSubtotalBreakNumber || ''}
+                value={indirectAccumulatedBreaked || '0'}
+                disabled
                 placeholder="N° Descanso"
                 text=""
-                onChange={(e) => {
-                  onChangeData(e)
-                }}
               />
             </CTableDataCell>
             <CTableDataCell>
               <CFormInput
                 type="text"
                 id="indirectSubtotalWorkNumber"
-                value={totalIndirectWorkForce.indirectSubtotalWorkNumber || ''}
+                value={indirectAccumulatedWorked || '0'}
+                disabled
                 placeholder="N° Obra"
                 text=""
-                onChange={(e) => {
-                  onChangeData(e)
-                }}
               />
             </CTableDataCell>
             <CTableDataCell>
               <CFormInput
                 type="text"
                 id="indirectSubtotalHHNumber"
-                value={totalIndirectWorkForce.indirectSubtotalHHNumber || ''}
+                value={indirectAccumulatedHours || '0'}
+                disabled
                 placeholder="HH (Turno)"
                 text=""
-                onChange={(e) => {
-                  onChangeData(e)
-                }}
               />
             </CTableDataCell>
           </CTableRow>
@@ -148,10 +170,10 @@ const TotalIndirectWorkForce = () => {
                 id="indirectPreviusAccumulated"
                 value={totalIndirectWorkForce.indirectPreviusAccumulated || ''}
                 placeholder="Total"
-                text=""
                 onChange={(e) => {
                   onChangeData(e)
                 }}
+                text=""
               />
             </CTableDataCell>
           </CTableRow>
@@ -161,13 +183,10 @@ const TotalIndirectWorkForce = () => {
               <CFormInput
                 type="text"
                 id="indirectCurrentAccumulated"
-                value={indirectAccumulatedHours || ''}
-                placeholder="Total"
+                value={indirectAccumulatedActual || '0'}
                 disabled
+                placeholder="Total"
                 text=""
-                onChange={(e) => {
-                  onChangeData(e)
-                }}
               />
             </CTableDataCell>
           </CTableRow>
