5654cbf0caa29dd699210fe136987d0d1d1fd136
Luis Guzmán <luisguzman@MacBook-Pro-de-Luis.local>
2024-09-13 12:42:29 -0300

    wip: trisemanal

--- src/hooks/useGetTrisemanalData.js
+++ src/hooks/useGetTrisemanalData.js
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import { useFetchTrisemanalData } from './useFetch'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import axios from 'axios'
+import axios, { HttpStatusCode } from 'axios'
+import useRegisterGeneralData from './useRegisterGeneralData'
 
 const useGetTrisemanalData = () => {
   const [error, setError] = useState()
   const [isError, setIsError] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
+  const [errorMutate, setErrorMutate] = useState()
   const queryClient = useQueryClient()
+  const { getProject, getContract } = useRegisterGeneralData()
+
+  const projectLS = JSON.parse(getProject())
+  const contractLS = JSON.parse(getContract())
 
   const { data, isLoading: loadingTrisemanal, error: errorTrisemanal } = useFetchTrisemanalData()
 
@@ -51,7 +57,49 @@ const useGetTrisemanalData = () => {
     },
   })
 
-  const uploadTrisemanal = (file) => {}
+  const registerMutation = useMutation({
+    mutationFn: async (newTodo) => {
+      return await axios
+        .post(
+          `${process.env.REACT_APP_BASE_URL}api/v1/trisemanal/upload-trisemanal/${projectLS.id}/${contractLS.id}`,
+          newTodo,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          },
+        )
+        .then((res) => {
+          if (res.status === HttpStatusCode.Created) {
+            setIsError(false)
+            return res.ok
+          } else {
+            setErrorMutate('Error al registrar proyecto')
+            setIsError(true)
+            return false
+          }
+        })
+        .catch((err) => {
+          setErrorMutate('Error al registrar proyecto')
+          setIsError(true)
+          return false
+        })
+    },
+    onSuccess: (suc) => {
+      queryClient.invalidateQueries({ queryKey: ['technical-documentation'] })
+    },
+    onError: (err) => {
+      setErrorMutate('Error al registrar proyecto')
+      setIsError(true)
+      return false
+    },
+  })
+
+  const uploadTrisemanal = (data) => {
+    setIsError(false)
+    const response = registerMutation.mutate(data)
+    return response
+  }
 
   return { data, loadingTrisemanal, errorTrisemanal, uploadTrisemanal }
 }
