376cc6a9eccc7d4f8fbf73b5b26473d330cc5717
Luis Guzmán <luisguzman@MacBook-Pro-de-Luis.local>
2024-11-03 22:16:56 -0300

    delete document

--- src/components/maintainers/tech-doc/TechnicalDocList.jsx
+++ src/components/maintainers/tech-doc/TechnicalDocList.jsx
@@ -17,7 +17,7 @@ const TechnicalDocList = () => {
   const { getData } = useGetCachedQueryData()
   const techDocQuery = getData('technical-documentation')
   const techDocCatQuery = getData('technical-documentation-categories')
-  const { deleteVehicle } = useTechnicalDoc()
+  const { deleteDoc } = useTechnicalDoc()
 
   return (
     <>
@@ -40,7 +40,7 @@ const TechnicalDocList = () => {
                   <CButton
                     className="btn-action-delete"
                     onClick={() => {
-                      deleteVehicle(doc.id)
+                      deleteDoc(doc.id)
                     }}
                   >
                     <CIcon icon={cilTrash} />
