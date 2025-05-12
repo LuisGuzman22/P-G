bb7422a72ab19acbdb4662ab3bffb3e8971b87bc
Luis Guzman <lguzman2207@gmail.com>
2025-05-12 19:58:29 -0400

    pdf viewer

--- src/components/gantt-chart/GanttChart.jsx
+++ src/components/gantt-chart/GanttChart.jsx
@@ -1,69 +1,34 @@
 import { React, useEffect, useState } from 'react'
 import { CCard, CCardBody, CButton } from '@coreui/react'
-import { Chart } from 'react-google-charts'
-
-import Skeleton from 'react-loading-skeleton'
-import { datamock } from './data'
-import useGantt from 'src/hooks/useGantt'
 
 const GanttChart = () => {
-  const options = {
-    height: 400,
-    gantt: {
-      trackHeight: 30,
-    },
-    legend: { position: 'none', fontSize: 13, italic: false },
-    annotations: {
-      textStyle: {
-        fontName: 'Times-Roman',
-        fontSize: 13,
-        bold: true,
-        italic: false,
-      },
-    },
-    tooltip: { isHtml: true },
-  }
-
-  const { data, isLoading, error, refetch, isRefetching } = useGantt()
-
-  const convertDatesInArray = (data) => {
-    return data.map((row) =>
-      row.map((value) =>
-        typeof value === 'string' && value.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/)
-          ? new Date(value)
-          : value,
-      ),
-    )
-  }
-
   return (
     <div className="proyect-administration">
       <h2>Carta Gantt</h2>
 
       <CCard className="action-buttons">
         <CCardBody>
-          {!isLoading && !isRefetching && data.rows.length > 0 ? (
-            <>
-              <Chart
-                chartType="Gantt"
-                width="100%"
-                height="50%"
-                data={[data.columns, ...convertDatesInArray(data.rows)]}
-                // data={datamock}
-                options={options}
-                chartLanguage="es-419"
-                tooltip={{
-                  trigger: 'selection',
-                  showColorCode: true,
-                  isHtml: true,
-                }}
-              />
-            </>
-          ) : (
-            <>
-              <Skeleton />
-            </>
-          )}
+          <object
+            data="https://cdn.simplepdf.com/simple-pdf/assets/sample.pdf"
+            type="application/pdf"
+            width="100%"
+            height="500px"
+          >
+            <iframe
+              src="https://cdn.simplepdf.com/simple-pdf/assets/sample.pdf"
+              width="100%"
+              height="100%"
+              // style="border: none;"
+            >
+              <p>
+                Your browser does not support PDFs.
+                <a href="https://cdn.simplepdf.com/simple-pdf/assets/sample.pdf">
+                  Download the PDF
+                </a>
+                .
+              </p>
+            </iframe>
+          </object>
         </CCardBody>
       </CCard>
     </div>
@@ -71,3 +36,5 @@ const GanttChart = () => {
 }
 
 export default GanttChart
+
+// https://cdn.simplepdf.com/simple-pdf/assets/sample.pdf
