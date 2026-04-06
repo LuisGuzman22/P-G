c151e17e01872a441f0561480f31369df1cf215e
Pablo <pablo.vera82@gmail.com>
2026-04-06 00:38:42 -0400

    fix: corrige errores de formato prettier en List.jsx

--- src/components/maintainers/weather/WeatherList.jsx
+++ src/components/maintainers/weather/WeatherList.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {
-  CButton,
-} from '@coreui/react'
+import { CButton } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilPencil, cilTrash } from '@coreui/icons'
 import useGetCachedQueryData from 'src/hooks/useGetCachedQueryData'
